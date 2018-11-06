ff95b47a773617594d4c3c00268b938aef415c82
pinhao1994@gmail.com <pinhao1994>
2018-11-06 16:55:26 -0500

    add example 4 selection

--- ADL/a4/index.js
+++ ADL/a4/index.js
@@ -12,7 +12,9 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.', 
+  'example4':
+      Life "out there" is so strange, so unique, so full of hardship and danger, and'     'yet so intensely interesting that it seems like another world.'
 };
 
 function status(statusText) {
